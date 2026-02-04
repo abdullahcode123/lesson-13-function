1dea59f1e10e5872fe16a28a80ea6382ba1d0faa
abdullahcoding123 <abdullahshahzadturk@gmail.com>
2026-02-04 18:59:38 +0500

    Function

--- wheather.py
+++ wheather.py
@@ -1,6 +1,10 @@
 def weather():
     print("Weather is pleasent in ",spring)
     print("same weather is in ",autumn)
+    print("weather is hot in ",winter)
+    print("weather i cold in ",summer)
 spring="autumn"
 autumn=spring
+winter="summer"
+summer="winter"
 weather()
